09dbd838f5644368887689769110314daee234dc
Suhail Shaik <shaiksuhail74818@gmail.com>
2025-07-22 10:16:22 +0530

    commit3

---
+++ Practice_Problems/p9_pass_or_fail.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main() {
+    int marks;
+    printf("Enter the marks");
+    scanf("%d",&marks);
+    if(marks>30){
+        print("you pass");
+    }else{
+        printf("you fail");
+    }
+    return 0;
+}
