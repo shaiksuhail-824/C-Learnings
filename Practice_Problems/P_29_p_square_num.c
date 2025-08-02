2ce03b61934cd180a95728c8223882cd97f7aee7
Suhail Shaik <shaiksuhail74818@gmail.com>
2025-08-02 23:20:07 +0530

    updated1

---
+++ Practice_Problems/P_29_p_square_num.c
@@ -0,0 +1,31 @@
+ # include <stdio.h>
+
+ void square(int n);
+
+ void _square(int *n);
+
+ int main() {
+    int num;
+    scanf("%d",&num);
+    printf("square numbers : %d \n", num);
+    square(num);
+
+    _square(&num);
+
+   printf("square numbers : %d \n", num);
+   return 0;
+    
+
+ }
+ //value Address
+ void square(int n) {
+    int result = n*n;
+    printf("square numbers : %d \n", result);
+      
+ }
+ // reference Address
+
+ void _square(int *n) {
+      *n = (*n) * (*n);
+      printf("square numbers : %d \n", *n);
+ }
